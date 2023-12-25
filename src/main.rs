cfeacd1ef3b07899176f0dbea8dd5eaf92dab709
Hikakkun <htrb0130@icloud.com>
2023-12-25 18:53:33 +0900

    コマンドライン引数から編集ファイルパスを取得できるように実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    // コマンドライン引数を取得する
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2{
+        eprintln!("Usage: {} <edit_file_path>", &args[0]);
+        std::process::exit(1);
+    }
+
+    let edit_file_path = &args[1];
+
+    println!("edit_file_path: {}", edit_file_path);
 }
