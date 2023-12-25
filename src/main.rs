008a116a77e038abec31b68028fa48b3998948ca
Hikakkun <htrb0130@icloud.com>
2023-12-25 19:44:03 +0900

    標準入力と整数パースを実装

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use std::fs::File;
+use std::io::{self, BufRead, Write};
 use std::env;
+
+fn read_file(edit_file_path: &str) -> io::Result<Vec<String>> {
+    let file = File::open(edit_file_path)?;    
+
+    let reader = io::BufReader::new(file);
+
+    let lines = reader.lines().collect::<Result<_, _>>()?;
+
+    return Ok(lines)
+}
+
+fn input(prompt: &str) -> String {
+    print!("{}", prompt);
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    input.trim().to_string()
+}
 fn main() {
     // コマンドライン引数を取得する
     let args: Vec<String> = env::args().collect();
@@ -11,4 +32,25 @@ fn main() {
     let edit_file_path = &args[1];
 
     println!("edit_file_path: {}", edit_file_path);
+
+    let lines = match read_file(edit_file_path) {
+        Ok(lines) => lines,
+        Err(err) => {
+            eprintln!("Error reading file: {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    println!("Contents");
+    lines.iter().enumerate().for_each(|(index, line)|{
+        println!("{:02}|{}", index, line)
+    });
+
+    let edit_line_number = match input("input edit line number>").trim().parse::<i32>() {
+        Ok(number) => number,
+        Err(_) => {
+            eprintln!("Invalid input for line number");
+            std::process::exit(1);
+        }        
+    };
 }
